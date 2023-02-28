7393bc20b999d43be038ce697032f6d4a4d20244
peterwakoko <peterwakoko@gmail.com>
2023-02-28 09:40:50 +0300

    added week's special text

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -13,9 +13,34 @@ function Homepage() {
         <OnlineMenuButton />
       </div>
       <div className="delivery-flex">
-        <div>Greed salad</div>
-        <div>Bruchetta</div>
-        <div>Lemon Dessert</div>
+        <div>
+          Greed salad $12.99
+          <p>The famous greek salad of</p>
+          <p>crispy lettuce, peppers, olives</p>
+          <p>and our Chicago style fets</p>
+          <p>cheese, garnished with</p>
+          <p>crunchy garlic and rosemary</p>
+          <p>crautons</p>
+          <span>Order a delivery</span>
+        </div>
+        <div>
+          Bruchetta $5.99
+          <p>Our Bruschetta is made from</p>
+          <p>grilled breed that has been</p>
+          <p>smeared with garlic and</p>
+          <p>seasoned with salt and olive</p>
+          <p>oil</p>
+          <span>Order a delivery</span>
+        </div>
+        <div>
+          Lemon Dessert $5.00
+          <p>This comes straight from</p>
+          <p>grandma's recipe book,every</p>
+          <p>every last ingredient has been</p>
+          <p>sourced and is as authentic</p>
+          <p>as can be imagined.</p>
+          <span>Order a delivery</span>
+        </div>
       </div>
     </>
   );
