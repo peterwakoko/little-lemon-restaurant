87b3f9b6d53bc364b3dd3ba5ad0da35ebc26ff41
peterwakoko <peterwakoko@gmail.com>
2023-03-02 08:38:20 +0300

    created a bookingForm component

--- src/pages/Reservationspage.js
+++ src/pages/Reservationspage.js
@@ -1,8 +1,10 @@
+import BookingForm from "../components/BookingForm";
 function Reservationspage() {
   return (
     <>
       <div className="main-flex">
         <h1>Reservationspage</h1>
+        <BookingForm />
       </div>
     </>
   );
