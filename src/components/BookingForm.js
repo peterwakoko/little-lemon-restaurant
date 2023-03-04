fcc3e900616de89cd3b0ad80cc0c1f1d6522cd31
peterwakoko <peterwakoko@gmail.com>
2023-03-04 07:53:27 +0300

    updated bookingForm

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,22 +1,52 @@
+import { useState } from "react";
 export default function BookingForm() {
+  const [date, setDate] = useState("");
+  const [availableTimes, setAvailableTimes] = useState([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
+  const [numberOfGuests, setNumberOfGuests] = useState("1");
+  const [occasion, setOcassion] = useState("Birthday");
+  function handleChange(e) {
+    setDate(e.target.value);
+    setAvailableTimes(e.target.value);
+    setNumberOfGuests(e.target.value);
+    setOcassion(e.target.value);
+  }
   return (
     <>
-      <form style="display: grid; max-width: 200px; gap: 20px">
+      <form style={{ display: "grid", maxWidth: "200px", gap: "20px" }}>
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
+        <input type="date" id="res-date" value={date} onChange={handleChange} />
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time ">
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
+        <select
+          id="res-time "
+          value={availableTimes[0]}
+          onChange={handleChange}
+        >
+          <option>{availableTimes[0]}</option>
+          <option>{availableTimes[1]}</option>
+          <option>{availableTimes[2]}</option>
+          <option>{availableTimes[3]}</option>
+          <option>{availableTimes[4]}</option>
+          <option>{availableTimes[5]}</option>
         </select>
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <input
+          type="number"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+          value={numberOfGuests}
+          onChange={handleChange}
+        />
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
+        <select id="occasion" value={occasion} onChange={handleChange}>
           <option>Birthday</option>
           <option>Anniversary</option>
         </select>
